7a18752c7f3cb4c57f6c7ce6ab157104ba1743d8
Hemant Nigam <hemant.nigam@nagarro.com>
2020-07-27 20:32:16 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import { Provider } from "react-redux";
+import "materialize-css/dist/css/materialize.min.css";
+import store from "./Components/Store/Store";
+import ProtectedRoute from "./ProtectedRoute";
+import Home from "./Components/Home/Home";
+import Auth from "./Auth";
+import MainRoute from "./MainRoute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Auth>
+        <div className="App">
+          <Router>
+            <Header />
+            <Switch>
+              <MainRoute exact path="/" />
+              <ProtectedRoute path="/home" component={Home} />
+              <Redirect from="*" to="/" />
+            </Switch>
+          </Router>
+        </div>
+      </Auth>
+    </Provider>
   );
 }
 
